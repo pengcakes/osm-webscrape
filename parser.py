54171d778f1f0b12092fdd2e9f6aab4dd8d11369
azplap <austinpeng97@gmail.com>
2018-09-10 20:06:15 -0400

    Finished parser

--- parser.py
+++ parser.py
@@ -1,10 +1,40 @@
+"""
+Search for:
+<way> --> <tag k = "building:height" v = "184">. </way>
+k="building:height"
+k="building:levels"
+"""
 import xml.etree.ElementTree as ET
 
 file_name = 'searsT.osm'
+#file_name = 'movies.xml'
 
 tree = ET.parse(file_name)
-
 root = tree.getroot()
+
+building_heights = []
+building_names = []
+building_dict = {}
+
+#find building height
+for child in root.findall("way/tag[@k='building:height']"):
+	building_heights.append(child.attrib['v'])
+
+#get building names
+for child in root.findall("way/tag[@k='building:height'].../tag[@k='name']"):
+	building_names.append(child.attrib['v'])
+
+
+
+for i in range(len(building_heights)):
+	building_dict[building_names[i]] = building_heights[i]
+
+for x in building_dict:
+	print(x, building_dict[x])
+
+
+##################################################################
+# SANDBOX
 
 # ###iterate through children
 # for child in root:
@@ -20,12 +50,8 @@ root = tree.getroot()
 # for x in root.iter("way"):
 # 	print(x.attrib)
 
-
-
 ####PRINT ALL
 #print(ET.tostring(root, encoding='utf8').decode('utf8'))
-
-
 
 ####SELECT BASED ON TAG
 
@@ -33,14 +59,12 @@ root = tree.getroot()
 #     print(movie.attrib)
 
 # for movie in root.findall("./genre/decade/movie/format/[@multiple='Yes']"):
-#     print(movie.attrib, movie.text)
+# 	myList.append(movie)
+# 	print(movie.attrib, movie.text)
 
 # for movie in root.findall("./genre/decade/movie/format/[@multiple='Yes']..."):
 #     print(movie.attrib)
 
-####################################RETURN ALL BUILDING HEIGHTS
-for x in root.findall("./way/tag/[building:height]"):
-	print(x.attrib)
 
 ####MODIFYING
 # b2tf = root.find("./genre/decade/movie[@title='Back 2 the Future']")
