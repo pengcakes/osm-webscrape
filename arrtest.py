aedd7360f117d9533bc1e2a86383db7f1deec21d
azplap <austinpeng97@gmail.com>
2018-09-21 22:41:56 -0400

    finished spaget version

---
+++ arrtest.py
@@ -0,0 +1,9 @@
+#test multidimensional arrays
+
+
+
+my_dict = {
+	"my_key": [1, 2, 3]
+};
+print (my_dict["my_key"][0])
+print (my_dict["my_key"][1])
--- parser.py
+++ parser.py
@@ -3,6 +3,13 @@ Search for:
 <way> --> <tag k = "building:height" v = "184">. </way>
 k="building:height"
 k="building:levels"
+
+DATASET:
+
+BUILDING NAME: [id, height, lat[], long[]]
+
+FIND WAY TO MATH ND REFS TO BUILDING ID
+
 """
 import xml.etree.ElementTree as ET
 
@@ -12,25 +19,81 @@ file_name = 'searsT.osm'
 tree = ET.parse(file_name)
 root = tree.getroot()
 
+building_dict = {}
+building_data = []
+
+building_id = []
 building_heights = []
 building_names = []
-building_dict = {}
-
-#find building height
-for child in root.findall("way/tag[@k='building:height']"):
-	building_heights.append(child.attrib['v'])
-
-#get building names
-for child in root.findall("way/tag[@k='building:height'].../tag[@k='name']"):
-	building_names.append(child.attrib['v'])
+building_locations = []
+nodes = []
 
 
+#not optimized even a little, spaget but it works
+def nd_parse():
 
-for i in range(len(building_heights)):
-	building_dict[building_names[i]] = building_heights[i]
+	node_ref = []
+	count = 0
+	for i in building_id:
+		if(count>0):
+			node_ref.append(temp_list)
+		count+=1
+		temp_list = []
+		for x in root.findall("way[@id='{buidling_id}']/nd".format(buidling_id=i)):
+			temp_list.append(x.attrib['ref'])
+			
+	return node_ref
 
+#holy big o^3
+def get_latlong():
+	var = nd_parse()
+	latitude = []
+	longitude = []
+	count = 0
+	for i in var:
+		for x in i:
+			for j in root.findall("node[@id='{z}']".format(z = x)):
+				latitude.append((j.attrib['lat']))
+				longitude.append((j.attrib['lon']))
+
+	return latitude, longitude
+
+
+def parse():
+	#get building height
+	for x in root.findall("way/tag[@k='building:height']"):
+		building_heights.append(x.attrib['v'])
+
+	#get building names
+	for x in root.findall("way/tag[@k='building:height'].../tag[@k='name']"):
+		building_names.append(x.attrib['v'])
+
+	#get building id
+	for x in root.findall("way/tag[@k='building:height']..."):
+		building_id.append(x.attrib['id'])
+
+	temp = get_latlong()
+
+	#add data to building_dict
+	for x in range(len(building_heights)):
+		building_dict[building_names[x]] = [building_id[x], building_heights[x], temp[0][x], temp[1][x]]
+
+	return building_dict
+
+
+parse()
 for x in building_dict:
 	print(x, building_dict[x])
+
+# var = nd_parse()
+
+# test = var[0][0]
+
+# print(len(var))
+
+# for x in root.findall("node[@id='{z}']".format(z = test)):
+# 	print(x.attrib['lat'])
+# 	print(x.attrib['lon'])
 
 
 ##################################################################
